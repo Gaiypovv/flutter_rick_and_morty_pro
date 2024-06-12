0e2db7c86d49cdfde48bbdda7bb9bdbce98955e0
Eldar Gaiypov <eldargaiypov@gmail.com>
2024-06-12 16:07:09 +0600

    add new page after splash screen

--- lib/presentetation/screens/splash_screen.dart
+++ lib/presentetation/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rick_and_morty_pro/presentetation/screens/home_screen.dart';
 import 'package:flutter_rick_and_morty_pro/resources/resources.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -10,10 +11,29 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    routing();
+    super.initState();
+  }
+
+  Future<void> routing() async {
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const HomeScreen(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Image.asset(
         AppPngs.splash,
+        fit: BoxFit.fill,
       ),
     );
   }
