b7137c076a3265af06c80ab59d7c326dc969bd38
Eldar Gaiypov <eldargaiypov@gmail.com>
2024-06-12 14:38:18 +0600

    added packages and set them

---
+++ lib/resources/app_pngs.dart
@@ -0,0 +1,10 @@
+part of 'resources.dart';
+
+class AppPngs {
+  AppPngs._();
+
+  static const String nativeSplash = 'assets/pngs/NativeSplash.png';
+  static const String splash = 'assets/pngs/Splash.png';
+  static const String filterNotFounded = 'assets/pngs/filter_not_founded.png';
+  static const String notFound = 'assets/pngs/not_found.png';
+}
