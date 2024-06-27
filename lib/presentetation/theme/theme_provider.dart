67a8297e84532de07af49f9fa60a5f5b00ab1905
Eldar Gaiypov <eldargaiypov@gmail.com>
2024-06-27 11:36:56 +0600

    added backgraound color theme

--- lib/presentetation/theme/theme_provider.dart
+++ lib/presentetation/theme/theme_provider.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rick_and_morty_pro/presentetation/theme/app_colors.dart';
 
 class ThemePovider extends ChangeNotifier {
+  bool isDarkTheme = true;
+
   ThemeData theme = ThemeData(scaffoldBackgroundColor: AppColors.darkBgColor);
 
-  void changeTheme() {}
+  void changeTheme() {
+    isDarkTheme = !isDarkTheme;
+    theme = ThemeData(
+        scaffoldBackgroundColor:
+            isDarkTheme ? AppColors.darkBgColor : AppColors.lightBgColor);
+    notifyListeners();
+  }
 }
